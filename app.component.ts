44bfb972f567b3187025fb01dafbafa44e77761f
luismenslin <luis.fmenslinx@gmail.com>
2024-07-08 13:14:31 -0300

    first

---
+++ app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from "./components/first-component/first-component.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, FirstComponentComponent]
+})
+export class AppComponent {
+  title = 'angular';
+}
